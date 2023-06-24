1f4feea0c3093a4c299f032cec14f5002573fb7a
Marcin-Matczak <marcorn.88@gmail.com>
2023-06-24 14:46:42 +0200

    Smooth page navigation added

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
+/* Elements */
 
+const buttonLearnMore = document.querySelector('.btn--learn-more');
+const sectionOne = document.querySelector('#section-1');
+const navigationList = document.querySelector('.navigation__list');
+
+/* Smooth scrolling page navigation */
+
+buttonLearnMore.addEventListener('click', function () {
+  sectionOne.scrollIntoView({ behavior: 'smooth' });
+});
+
+navigationList.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('navigation__link')) return;
+  const id = e.target.getAttribute('href');
+  document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+});
