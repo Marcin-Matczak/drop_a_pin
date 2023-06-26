b57929640b39141c914c2fce5c9c080d03419acf
Marcin-Matczak <marcorn.88@gmail.com>
2023-06-26 10:05:51 +0200

    Added support for tabs in features section

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,13 @@
 /* Elements */
 
 const buttonLearnMore = document.querySelector('.btn--learn-more');
+
 const sectionOne = document.querySelector('#section-1');
 const navigationList = document.querySelector('.navigation__list');
+
+const tabs = document.querySelectorAll('.btn--tab');
+const tabsContainer = document.querySelector('.features__tab-container');
+const tabsContent = document.querySelectorAll('.features__content');
 
 /* Smooth scrolling page navigation */
 
@@ -15,4 +20,20 @@ navigationList.addEventListener('click', function (e) {
   if (!e.target.classList.contains('navigation__link')) return;
   const id = e.target.getAttribute('href');
   document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+});
+
+/* Features tabs */
+
+tabsContainer.addEventListener('click', function (e) {
+  tabs.forEach(tab => tab.classList.remove('btn--tab-active'));
+  e.target.classList.add('btn--tab-active');
+
+  const articleNumber = e.target.getAttribute('data-tab');
+  const article = document.querySelector(
+    `.features__content--${articleNumber}`
+  );
+  tabsContent.forEach(article =>
+    article.classList.remove('features__content--active')
+  );
+  article.classList.add('features__content--active');
 });
